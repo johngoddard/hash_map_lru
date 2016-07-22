be1441b3d4e1cc64d10dd96de2b89319be4b4057
John Goddard <johngoddard243@gmail.com>
2016-07-22 16:55:45 -0700

    dynamic array

--- skeleton/lib/p07_dynamic_array_bonus.rb
+++ skeleton/lib/p07_dynamic_array_bonus.rb
@@ -25,6 +25,8 @@ class StaticArray
 end
 
 class DynamicArray
+  include Enumerable
+
   attr_reader :count
 
   def initialize(capacity = 8)
@@ -33,9 +35,26 @@ class DynamicArray
   end
 
   def [](i)
+    return nil if i > @store.length-1 ||  @count +i < 0
+    if i < 0
+      @store[@count + i]
+    else
+      @store[i]
+    end
+
   end
 
   def []=(i, val)
+    if i < 0
+      @store[@count + i] = val
+    else
+      if i > @count
+        push(nil) until @count == i
+        push(val)
+      else
+        @store[i] = val
+      end
+    end
   end
 
   def capacity
@@ -43,27 +62,60 @@ class DynamicArray
   end
 
   def include?(val)
+    each{|el| return true if el == val}
+    false
   end
 
   def push(val)
+    resize! if @count == @store.length
+    @store[@count] = val
+    @count += 1
   end
 
   def unshift(val)
+    resize! if @count == @store.length
+
+    (@count-1).downto(0).each do |i|
+      @store[i+1] = @store[i]
+    end
+    @store[0] = val
+    @count += 1
   end
 
   def pop
+    return nil if @count == 0
+    @count -= 1
+    to_return = @store[@count]
+    @store[@count] = nil
+    to_return
   end
 
   def shift
+    return nil if @count == 0
+    to_return = @store[0]
+
+    (1...@count).each do |idx|
+      @store[idx - 1] = @store[idx]
+    end
+
+    @store[count - 1] = nil
+    @count -= 1
+
+    to_return
   end
 
   def first
+    @store[0]
   end
 
   def last
+    @store[@count - 1]
   end
 
   def each
+    (0...@count).each do |idx|
+      yield(@store[idx])
+    end
   end
 
   def to_s
@@ -72,7 +124,11 @@ class DynamicArray
 
   def ==(other)
     return false unless [Array, DynamicArray].include?(other.class)
-    # ...
+
+    (0...@count).each do |idx|
+      return false unless @store[idx] == other[idx]
+    end
+
   end
 
   alias_method :<<, :push
@@ -81,5 +137,13 @@ class DynamicArray
   private
 
   def resize!
+    new_capacity = @count * 2
+    new_store = StaticArray.new(new_capacity)
+
+    (0...@count).each do |i|
+      new_store[i] = @store[i]
+    end
+
+    @store = new_store
   end
 end
